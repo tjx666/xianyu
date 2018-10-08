e283fb42abb1094d0daf6c207f6281800867df3c
tjx666 <2713151713@qq.com>
2018-10-08 18:05:56 +0800

    Component NavCard2

--- src/components/navCard2Item/NavCard2Item.jsx
+++ src/components/navCard2Item/NavCard2Item.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import IconFont from '../iconFont/IconFont';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import './navCard2Item.scss';
 
 export class NavCard2Item extends Component {
     static propTypes = {
-        
+        iconType: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        titleColor: PropTypes.string.isRequired,
     }
 
     render() {
         return (
             <div className="nav-card2-item">
-                <div className="item2-title-container">
+                <div className="item2-title-container" style={{ color: this.props.titleColor }}>
                     <IconFont
+                        className="item2-icon"
                         iconType={this.props.iconType}
                     />
                     <h2 className="item2-title">{this.props.title}</h2>
